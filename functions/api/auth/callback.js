a35c485ebae4129714678b1e28926fdc76e34bc6
Will Hare <will.hare.89@gmail.com>
2026-06-03 00:09:27 -0400

    Fix OAuth callback HTML

--- functions/api/auth/callback.js
+++ functions/api/auth/callback.js
@@ -27,46 +27,31 @@ export async function onRequestGet(context) {
       return new Response(`OAuth error: ${tokenData.error_description}`, { status: 400 });
     }
 
-    const script = `
-<!DOCTYPE html>
-const script = `
-<!DOCTYPE html>
-<html>
-<body>
-<script>
-(function() {
-  var token = ${JSON.stringify(tokenData.access_token)};
-  var provider = 'github';
-  
-  function attemptPostMessage() {
-    if (window.opener) {
-      window.opener.postMessage(
-        'authorization:' + provider + ':success:' + JSON.stringify({ token: token, provider: provider }),
-        '*'
-      );
-      setTimeout(function() { window.close(); }, 1000);
-    } else {
-      // Fallback: store in localStorage for the CMS to pick up
-      localStorage.setItem('netlify-cms-auth', JSON.stringify({ token: token, provider: provider }));
-      document.body.innerHTML = '<p style="font-family:sans-serif;padding:2rem;">Authenticated. You can close this window.</p>';
-    }
-  }
-  
-  attemptPostMessage();
-  window.addEventListener('message', function(e) {
-    attemptPostMessage();
-  }, false);
-})();
-<\/script>
-</body>
-</html>`;
+    const token = tokenData.access_token;
+    const provider = 'github';
+    const message = JSON.stringify({ token, provider });
 
-    return new Response(script, {
-  headers: { 
-    'Content-Type': 'text/html',
-    'Content-Security-Policy': "script-src 'unsafe-inline'",
-  },
-});
+    const html = '<!DOCTYPE html><html><body><script>(function() {'
+      + 'var token = ' + JSON.stringify(token) + ';'
+      + 'var provider = "github";'
+      + 'var msg = "authorization:" + provider + ":success:" + JSON.stringify({ token: token, provider: provider });'
+      + 'function send() {'
+      + '  if (window.opener) {'
+      + '    window.opener.postMessage(msg, "*");'
+      + '    setTimeout(function() { window.close(); }, 1000);'
+      + '  }'
+      + '}'
+      + 'send();'
+      + 'window.addEventListener("message", send, false);'
+      + '})();<\/script></body></html>';
+
+    return new Response(html, {
+      headers: {
+        'Content-Type': 'text/html',
+        'Content-Security-Policy': "script-src 'unsafe-inline'",
+      },
+    });
+
   } catch (err) {
     return new Response(`Server error: ${err.message}`, { status: 500 });
   }
